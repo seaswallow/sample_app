f42220e82a90a638e9d959952d6f44afb07da1f5
swallow321 <swallow321@126.com>
2015-04-13 19:06:20 -0700

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello do
+  def hello
   	  render text: "Hello World!"
   end
 end
